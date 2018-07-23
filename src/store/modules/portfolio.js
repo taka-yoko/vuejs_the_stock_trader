44ee82a28764f5f58fc70280e3b7ebabb95e771a
横山貴芳 <t-yoko@t-yoko.local>
2018-07-23 21:37:10 +0900

    Displaying the Funds

--- src/store/modules/portfolio.js
+++ src/store/modules/portfolio.js
@@ -14,6 +14,7 @@ const mutations = {
         quantity: quantity
       });
     }
+    state.funds -= stockPrice * quantity;
   },
   'SELL_STOCK' (state, {stockId, quantity, stockPrice}) {
     const record = state.stocks.find(element => element.id == stockId);
